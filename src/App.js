a4317f6654d2ae040590bea98647ac81fe752b16
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2021-12-14 18:14:47 -0300

    p

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import Button from './components/button/button';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Button from "./components/button/button";
 
 function App() {
   return (
-    <div className="App" id='app'>
+    <div className="App" id="app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-       
-      <Button label='wenasssssssssssssssssssssssssss'></Button>
-      <br />
+        <a
+          href="https://franciscog98.github.io/portfolio/"
+          target="__blank"
+          rel="noopener noreferrer"
+        >
+          <Button label="clic here"></Button>
+        </a>
+        <code>im coding react bitchie</code>
+        <Button label="clic me"></Button> <br />
       </header>
     </div>
   );
--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import './buttonStyles.css'
+import React from "react";
+import "./buttonStyles.css";
 
- const Button = ({ label }) => {
-    return (
-        <div data-testid="button" className='btn-primary' >
-            {label}
-        </div>
-    )
-}
+const Button = ({ label }) => {
+  return (
+    <div data-testid="button" className="btn-primary">
+      {label}
+    </div>
+  );
+};
 
-export default  Button
-
+export default Button;
