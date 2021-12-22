b5ce07e98bf0c0fd8b90980a4b9a9a69b29bf06c
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2021-12-22 10:56:53 -0300

    card component

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./cardStyles.css"
+
+const Card = () => {
+    return (
+        <div className='card-container'>
+            <h3 className='card-title'>CARD</h3>
+            <div className='card-content'>IMG</div>
+            <p className='card-info'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum deleniti mollitia commodi nobis nihil similique esse unde iure, nostrum id libero veniam suscipit, culpa dolores quidem odit consequatur consectetur facilis?
+                Porro praesentium quod totam soluta ad maiores ea sit impedit architecto. Aut sit maiores voluptatem, similique commodi magnam! Ad quas doloremque vel a quam est? Architecto maiores pariatur quibusdam quis.
+            </p>
+        </div>
+    )
+}
+
+export default Card
